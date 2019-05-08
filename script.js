4e098ca6deb745f2b7a9c67aefe2755994550316
Randon Calderwood <randon.calderwood@gmail.com>
2019-05-07 20:48:42 -0600

    done

--- script.js
+++ script.js
@@ -19,5 +19,27 @@ window.onload = function(){
     })
 
 
+    var isWorkShown = false;
+
+    document.getElementById("work_title").addEventListener('click', function(){
+        if(!isWorkShown)
+        {
+            document.getElementById("work_list").style.display = "block";
+            document.getElementById('work_title').innerHTML = "Hide Work Experience";
+            isWorkShown = true;
+        }
+        else
+        {
+            document.getElementById("work_list").style.display = "none";
+            document.getElementById('work_title').innerHTML = "Show Work Experience";
+            isWorkShown = false;
+        }
+
+
+    })
+
+
 
 }
+
+
